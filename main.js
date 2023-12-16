fef4bdaee108a6b3514d5c47f1db8dfbc38904d0
Will Maier <189851+whilp@users.noreply.github.com>
2023-12-16 13:09:28 -0800

    .

--- main.js
+++ main.js
@@ -1,37 +1,25 @@
 // import sqlite3InitModule from '../index.mjs';
 
-const container = document.querySelector('.main-thread');
-
-const logHtml = function (cssClass, ...args) {
-  const div = document.createElement('div');
-  if (cssClass) div.classList.add(cssClass);
-  div.append(document.createTextNode(args.join(' ')));
-  container.append(div);
-};
-
-const log = (...args) => logHtml('', ...args);
-const error = (...args) => logHtml('error', ...args);
-
 const start = function (sqlite3) {
-  log('Running SQLite3 version', sqlite3.version.libVersion);
+  console.log('Running SQLite3 version', sqlite3.version.libVersion);
   const db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
-  log('Created transient database', db.filename);
+  console.log('Created transient database', db.filename);
 
   try {
-    log('Creating a table...');
+    console.log('Creating a table...');
     db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
-    log('Insert some data using exec()...');
+    console.log('Insert some data using exec()...');
     for (let i = 20; i <= 25; ++i) {
       db.exec({
         sql: 'INSERT INTO t(a,b) VALUES (?,?)',
         bind: [i, i * 2],
       });
     }
-    log('Query data with exec()...');
+    console.log('Query data with exec()...');
     db.exec({
       sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
       callback: (row) => {
-        log(row);
+        console.log(row);
       },
     });
   } finally {
@@ -39,12 +27,12 @@ const start = function (sqlite3) {
   }
 };
 
-log('Loading and initializing SQLite3 module...');
+console.log('Loading and initializing SQLite3 module...');
 sqlite3InitModule({
   print: log,
   printErr: error,
 }).then((sqlite3) => {
-  log('Done initializing. Running demo...');
+  console.log('Done initializing. Running demo...');
   try {
     start(sqlite3);
   } catch (err) {
