edfb200ee4040e502ca4cd12cff94bec87737ef6
Will Maier <189851+whilp@users.noreply.github.com>
2023-12-16 13:26:49 -0800

    .

---
+++ script.js
@@ -0,0 +1,15 @@
+(async () => {
+  const worker = new Worker('worker.js', {
+    type: 'module',
+  });
+
+  worker.addEventListener('message', ({ data }) => {
+    switch (data.type) {
+      case 'log':
+        console.log(data.payload.cssClass, ...data.payload.args);
+        break;
+      default:
+        console.log('error', 'Unhandled message:', data.type);
+    }
+  });
+})();
