37e78c180c477dd165976775d20df49319a34fdd
Will Maier <189851+whilp@users.noreply.github.com>
2023-12-16 13:17:35 -0800

    .

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const container = document.querySelector('.worker');
-
-const logHtml = (cssClass, ...args) => {
-  const div = document.createElement('div');
-  if (cssClass) div.classList.add(cssClass);
-  div.append(document.createTextNode(args.join(' ')));
-  container.append(div);
-};
-
 (async () => {
   const worker = new Worker('worker.js', {
     type: 'module',
@@ -15,10 +6,10 @@ const logHtml = (cssClass, ...args) => {
   worker.addEventListener('message', ({ data }) => {
     switch (data.type) {
       case 'log':
-        logHtml(data.payload.cssClass, ...data.payload.args);
+        console.log(data.payload.cssClass, ...data.payload.args);
         break;
       default:
-        logHtml('error', 'Unhandled message:', data.type);
+        console.log('error', 'Unhandled message:', data.type);
     }
   });
 })();
--- worker.js
+++ worker.js
@@ -1,40 +1,30 @@
 // import sqlite3InitModule from '../index.mjs';
 
-const logHtml = function (cssClass, ...args) {
-  postMessage({
-    type: 'log',
-    payload: { cssClass, args },
-  });
-};
-
-const log = (...args) => logHtml('', ...args);
-const error = (...args) => logHtml('error', ...args);
-
 const start = function (sqlite3) {
-  log('Running SQLite3 version', sqlite3.version.libVersion);
+  console.log('Running SQLite3 version', sqlite3.version.libVersion);
   let db;
   if ('opfs' in sqlite3) {
     db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3');
-    log('OPFS is available, created persisted database at', db.filename);
+    console.log('OPFS is available, created persisted database at', db.filename);
   } else {
     db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
-    log('OPFS is not available, created transient database', db.filename);
+    console.log('OPFS is not available, created transient database', db.filename);
   }
   try {
-    log('Creating a table...');
+    console.log('Creating a table...');
     db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
-    log('Insert some data using exec()...');
+    console.log('Insert some data using exec()...');
     for (let i = 20; i <= 25; ++i) {
       db.exec({
         sql: 'INSERT INTO t(a,b) VALUES (?,?)',
         bind: [i, i * 2],
       });
     }
-    log('Query data with exec()...');
+    console.log('Query data with exec()...');
     db.exec({
       sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
       callback: (row) => {
-        log(row);
+        console.log(row);
       },
     });
   } finally {
@@ -42,15 +32,15 @@ const start = function (sqlite3) {
   }
 };
 
-log('Loading and initializing SQLite3 module...');
+console.log('Loading and initializing SQLite3 module...');
 sqlite3InitModule({
-  print: log,
-  printErr: error,
+  print: console.log,
+  printErr: console.error,
 }).then((sqlite3) => {
-  log('Done initializing. Running demo...');
+  console.log('Done initializing. Running demo...');
   try {
     start(sqlite3);
   } catch (err) {
-    error(err.name, err.message);
+    console.error(err.name, err.message);
   }
 });
